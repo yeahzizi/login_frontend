26af8137f45172dc7905c4dcd722cbda47b05449
yeahzizi <newyeahzizi@gmail.com>
2023-03-09 13:48:10 +0900

    이번에는 진짜 배포

---
+++ frontend/src/Components/Auth/AuthInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AuthInput() {
+  return <div />;
+}
+
+export default AuthInput;
---
+++ frontend/src/pages/Auth/Login.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Login() {
+  return <div>로그인 할거지 ehlfkrh</div>;
+}
+export default Login;
