9734be42337a84232d52cedcead4e3ba0bb16ce7
yeahzizi <newyeahzizi@gmail.com>
2023-03-09 10:24:39 +0900

    ci/cd 시도중

--- login/src/pages/Login.tsx
+++ login/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 function Login() {
-  return <div>로그인</div>;
+  return <div>로그인 할거지</div>;
 }
 export default Login;
--- login/src/pages/SignUp.tsx
+++ login/src/pages/SignUp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 function SignUp() {
-  return <div>회원가입</div>;
+  return <div>회원가입 할거지</div>;
 }
 export default SignUp;
